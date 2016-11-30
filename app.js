0eb6dd19c33d11cb42ebf04660e34199d1c97820
Shiv Unadkat <Shiv Unadkat>
2016-11-29 20:35:26 -0500

    The one

---
+++ app.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var question1 = prompt('Is Shiv a L33T Haxsor').toLowerCase();
+
+if (question1 === 'yes' || question1 === 'y') {
+  alert('HELL YEAH BABY, YOU HAVE TO ASSIMILATE THE MINDSET AND THEN THE CHARACTER FALLS INTO PLACE.');
+} else if (question1 === 'no' || question1 === 'n') {
+  alert('Well, that is true. Not yet at least. Given the determination who knows is possible?');
+} else {
+  alert('If you could please answer Yes/Y or No/N.');
+}
+console.log('Trying to motivate myself to get that dank software engineering level.')
+
+var question2 = prompt('Does Shiv Daddy chill with the powder puff girls?');
+var grandpaVoice = '...Back in my day';
+
+if (question2 === 'yes' || question2 === 'y') {
+  alert('I got that Mojo Jojo baby');
+} else if (question2 === 'no' || question2 === 'n') {
+  alert('Not anymore, but I remember (*Grandpa Voice*)' + grandpaVoice.italics());
+} else {
+  alert('If you could please answer Yes/Y or No/N.');
+}
+console.log('I used to watch the power puff girls back in the day.')
+
+var question3 = prompt('Was Shivvy Smalls the one that crawled on top of the empire state building?').toLowerCase();
+
+if (question3 === 'yes' || question3 === 'y') {
+  alert('Haha that was actually King Kong you Ding Dong!');
+} else if (question3 === 'no' || question3 === 'n') {
+  alert('Well obviously not you\'d have to be on drugs to think otherwise ;)');
+} else {
+  alert('If you could please answer Yes/Y or No/N.');
+}
+console.log('Was thinking about King Kong for some reason.')
+
+var question4 = prompt('Did Shivster manage to lose?').toLowerCase();
+
+if (question4 === 'yes' || question4 === 'y') {
+  alert('Well of course, because he didn\'t adequately prepare');
+} else if (question4 === 'no' || question4 === 'n') {
+  alert('That is incorrect. He fails constantly and continously. That is what breeds success.');
+} else {
+  alert('If you could please answer Yes/Y or No/N.');
+}
+console.log('Some motivational shit for myself.')
+
+var question5 = prompt('Will Shivzilla win?').toLowerCase();
+
+if (question5 === 'yes' || question5 === 'y') {
+  alert('There is no winning in this game. Only progression.');
+} else if (question5 === 'no' || question5 === 'n') {
+  alert('There is no winning in this game. Only progression.');
+} else {
+  alert('If you could please answer Yes/Y or No/N.');
+}
+console.log('Capitalism has bred a shallow definition of success. Our egos are too large with no true happiness to be found.')
